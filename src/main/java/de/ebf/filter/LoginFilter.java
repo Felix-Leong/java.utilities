414099d2ac6b644b280e8a8bc7c392605fe23f02
dwissk <dominik.wisskirchen@ebf.de>
2013-04-25 23:47:22 +0200

    Moves LoginFilter from telekom.billing to java.utilities.

---
+++ src/main/java/de/ebf/filter/LoginFilter.java
@@ -0,0 +1,78 @@
+package de.ebf.filter;
+
+import de.ebf.utils.HttpUtil;
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.log4j.Logger;
+
+public class LoginFilter implements Filter {
+
+   private static final Logger log = Logger.getLogger(LoginFilter.class);
+   private Pattern exclusionPattern;
+   private String contextPath;
+
+   @Override
+   public void init(FilterConfig config) throws ServletException {
+      String exclusions = config.getInitParameter("exclusions");
+      try {
+         exclusionPattern = Pattern.compile(exclusions);
+      } catch (PatternSyntaxException e) {
+         throw new RuntimeException("Could not compile LoginFilter exlusion pattern [" + exclusions + "]", e);
+      }
+   }
+
+   @Override
+   public void destroy() {
+      //empty
+   }
+
+   @Override
+   public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+      if (request instanceof HttpServletRequest) {
+         HttpServletRequest httpRequest = (HttpServletRequest) request;
+         contextPath = HttpUtil.getContextPath(httpRequest);
+         request.setAttribute("contextPath", contextPath);
+
+         HttpSession session = httpRequest.getSession();
+         String servletPath = httpRequest.getServletPath();
+         if (!exclusionPattern.matcher(servletPath).matches()) {
+            if (session == null) {
+               log.info("Redirecting [" + servletPath + "] to login page due to empty session.");
+               redirectToLogin(response);
+            } else {
+               Object user = session.getAttribute("user");
+               if (user == null) {
+                  String sessionRequestURI = (String) httpRequest.getSession().getAttribute("requestURI");
+                  if (sessionRequestURI == null) {
+                     String requestURI = httpRequest.getRequestURI();
+                     int index = requestURI.indexOf(contextPath);
+                     if (index != -1) {
+                        requestURI = requestURI.substring(index + contextPath.length());
+                     }
+                     httpRequest.getSession().setAttribute("requestURI", requestURI);
+                  }
+                  log.info("Redirecting [" + servletPath + "] to login page due to missing user session object");
+                  redirectToLogin(response);
+                  return;
+               }
+            }
+         }
+         filterChain.doFilter(request, response);
+      }
+   }
+
+   private void redirectToLogin(ServletResponse response) throws IOException {
+      HttpServletResponse httpResponse = (HttpServletResponse) response;
+      httpResponse.sendRedirect(contextPath + "/login");
+   }
+}
--- src/main/java/de/ebf/utils/HttpUtil.java
+++ src/main/java/de/ebf/utils/HttpUtil.java
@@ -5,12 +5,10 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
-
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import javax.servlet.http.HttpServletRequest;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
@@ -38,106 +36,109 @@ import org.apache.http.protocol.BasicHttpContext;
 import org.apache.log4j.Logger;
 
 public class HttpUtil {
-	
-	private static final Logger log = Logger.getLogger(HttpUtil.class);
-	
-	/* DEFAULT HTTP CLIENT VALUES */
-	private static final int DEFAULT_CONNECTION_TIMEOUT 			= 20000;
-	private static final boolean DEFAULT_FOLLOW_REDIRECTS 			= true;
-	
-	public static boolean isAvailable(String url, String user, String pass) throws Exception{
-		HttpResponse httpResponse = HttpUtil.get(url, user, pass);
-		if (httpResponse.getStatusLine().getStatusCode()==HttpStatus.SC_OK){
-        	return true;
-        }
-		return false;
-	}
-	
-	public static HttpResponse get(String url) throws Exception {
-		return get(url, null, null);
-	}
 
-	public static HttpResponse get(String url, String user, String pass) throws Exception{
-		URI uri = new URI(url);
-		if (uri.getHost()==null || uri.getScheme()==null){
-			throw new IllegalArgumentException("Incorrect URL "+url);
-		}
-		HttpGet httpGet2 = new HttpGet(uri);
+   private static final Logger log = Logger.getLogger(HttpUtil.class);
+   /* DEFAULT HTTP CLIENT VALUES */
+   private static final int DEFAULT_CONNECTION_TIMEOUT = 20000;
+   private static final boolean DEFAULT_FOLLOW_REDIRECTS = true;
 
-		DefaultHttpClient httpClient = getNewHttpClient();
-		
-	    try {
-	    	BasicHttpContext localcontext = new BasicHttpContext();
+   public static boolean isAvailable(String url, String user, String pass) throws Exception {
+      HttpResponse httpResponse = HttpUtil.get(url, user, pass);
+      if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+         return true;
+      }
+      return false;
+   }
 
-	    	if (!StringUtils.isEmpty(user) && !StringUtils.isEmpty(pass)){
-	    		httpClient.getCredentialsProvider().setCredentials(
-	                new AuthScope(httpGet2.getURI().getHost(), httpGet2.getURI().getPort()),
-	                new UsernamePasswordCredentials(user, pass));
-		        // Create AuthCache instance
-		        AuthCache authCache = new BasicAuthCache();
-		        // Generate BASIC scheme object and add it to the local auth cache
-		        BasicScheme basicAuth = new BasicScheme();
-		        authCache.put(new HttpHost(httpGet2.getURI().getHost()), basicAuth);
-		        
-		        // Add AuthCache to the execution context
-		        localcontext.setAttribute(ClientContext.AUTH_CACHE, authCache);
-			        
-	    	}
+   public static HttpResponse get(String url) throws Exception {
+      return get(url, null, null);
+   }
 
-	    	HttpResponse response = httpClient.execute(httpGet2, localcontext);
-            
-        	if (response.getStatusLine().getStatusCode()!=HttpStatus.SC_OK){
-        		log.warn("Got invalid HTTP response code for URL "+url+" "+response.getStatusLine().getStatusCode());
-        	}
-    		return response;
-	    } finally {
-	        // When HttpClient instance is no longer needed,
-	        // shut down the connection manager to ensure
-	        // immediate deallocation of all system resources
-	        //httpClient.getConnectionManager().shutdown();
-	    }
-	}
-	
-	private static DefaultHttpClient getNewHttpClient() {
-		SSLContext sslContext;
-		try {
-			sslContext = SSLContext.getInstance("SSL");
-			// set up a TrustManager that trusts everything
-			sslContext.init(null, new TrustManager[] { new X509TrustManager() {
-				public X509Certificate[] getAcceptedIssuers() {
-					return null;
-				}
-				
-				public void checkClientTrusted(X509Certificate[] certs, String authType) {
-				}
-				
-				public void checkServerTrusted(X509Certificate[] certs,	String authType) {
-				}
-			} }, new SecureRandom());
-			
-			SSLSocketFactory sf = new SSLSocketFactory(sslContext);
-			SchemeRegistry schemeRegistry = new SchemeRegistry();
-			schemeRegistry.register(new Scheme("https", 443, sf));
-			schemeRegistry.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
-						
-			ClientConnectionManager cm = new BasicClientConnectionManager(schemeRegistry);
-			DefaultHttpClient httpClient = new DefaultHttpClient(cm);
-			
-			final HttpParams params = new BasicHttpParams();
-			HttpClientParams.setRedirecting(params, DEFAULT_FOLLOW_REDIRECTS);
-			HttpClientParams.setCookiePolicy(params, CookiePolicy.IGNORE_COOKIES);
-			HttpConnectionParams.setConnectionTimeout(params, DEFAULT_CONNECTION_TIMEOUT);
-			HttpConnectionParams.setSoTimeout(params, DEFAULT_CONNECTION_TIMEOUT);
-			httpClient.setParams(params);
-			return httpClient;
-		} catch (NoSuchAlgorithmException | KeyManagementException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
+   public static HttpResponse get(String url, String user, String pass) throws Exception {
+      URI uri = new URI(url);
+      if (uri.getHost() == null || uri.getScheme() == null) {
+         throw new IllegalArgumentException("Incorrect URL " + url);
+      }
+      HttpGet httpGet2 = new HttpGet(uri);
 
-	public static String getBaseUrl(HttpServletRequest request) {
-		return request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath();
-	}
+      DefaultHttpClient httpClient = getNewHttpClient();
+
+      try {
+         BasicHttpContext localcontext = new BasicHttpContext();
+
+         if (!StringUtils.isEmpty(user) && !StringUtils.isEmpty(pass)) {
+            httpClient.getCredentialsProvider().setCredentials(
+                    new AuthScope(httpGet2.getURI().getHost(), httpGet2.getURI().getPort()),
+                    new UsernamePasswordCredentials(user, pass));
+            // Create AuthCache instance
+            AuthCache authCache = new BasicAuthCache();
+            // Generate BASIC scheme object and add it to the local auth cache
+            BasicScheme basicAuth = new BasicScheme();
+            authCache.put(new HttpHost(httpGet2.getURI().getHost()), basicAuth);
+
+            // Add AuthCache to the execution context
+            localcontext.setAttribute(ClientContext.AUTH_CACHE, authCache);
+
+         }
+
+         HttpResponse response = httpClient.execute(httpGet2, localcontext);
+
+         if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+            log.warn("Got invalid HTTP response code for URL " + url + " " + response.getStatusLine().getStatusCode());
+         }
+         return response;
+      } finally {
+         // When HttpClient instance is no longer needed,
+         // shut down the connection manager to ensure
+         // immediate deallocation of all system resources
+         //httpClient.getConnectionManager().shutdown();
+      }
+   }
+
+   private static DefaultHttpClient getNewHttpClient() {
+      SSLContext sslContext;
+      try {
+         sslContext = SSLContext.getInstance("SSL");
+         // set up a TrustManager that trusts everything
+         sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+               public X509Certificate[] getAcceptedIssuers() {
+                  return null;
+               }
+
+               public void checkClientTrusted(X509Certificate[] certs, String authType) {
+               }
+
+               public void checkServerTrusted(X509Certificate[] certs, String authType) {
+               }
+            }}, new SecureRandom());
+
+         SSLSocketFactory sf = new SSLSocketFactory(sslContext);
+         SchemeRegistry schemeRegistry = new SchemeRegistry();
+         schemeRegistry.register(new Scheme("https", 443, sf));
+         schemeRegistry.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
+
+         ClientConnectionManager cm = new BasicClientConnectionManager(schemeRegistry);
+         DefaultHttpClient httpClient = new DefaultHttpClient(cm);
+
+         final HttpParams params = new BasicHttpParams();
+         HttpClientParams.setRedirecting(params, DEFAULT_FOLLOW_REDIRECTS);
+         HttpClientParams.setCookiePolicy(params, CookiePolicy.IGNORE_COOKIES);
+         HttpConnectionParams.setConnectionTimeout(params, DEFAULT_CONNECTION_TIMEOUT);
+         HttpConnectionParams.setSoTimeout(params, DEFAULT_CONNECTION_TIMEOUT);
+         httpClient.setParams(params);
+         return httpClient;
+      } catch (NoSuchAlgorithmException | KeyManagementException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return null;
+   }
+
+   public static String getBaseUrl(HttpServletRequest request) {
+      return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + getContextPath(request);
+   }
+
+   public static String getContextPath(HttpServletRequest request) {
+      return (request.getContextPath() == null) ? "" : request.getContextPath();
+   }
 }
