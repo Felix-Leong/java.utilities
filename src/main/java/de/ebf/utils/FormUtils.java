cfeefb49790b54d028566196180f47ce4b7287a0
dwissk <dominik.wisskirchen@ebf.de>
2013-08-16 07:23:52 +0200

    [Fix] NPE

--- src/main/java/de/ebf/utils/FormUtils.java
+++ src/main/java/de/ebf/utils/FormUtils.java
@@ -2,22 +2,25 @@ package de.ebf.utils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.lang.StringUtils;
 
 public class FormUtils {
-	
-	private static final String EMAIL_PATTERN = 
-			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-	
-	private static Pattern pattern;
-	private static Matcher matcher; 
-	
-	static {
-		pattern = Pattern.compile(EMAIL_PATTERN);
-	}
-	
-	public static boolean isEmail(String email){
-		matcher = pattern.matcher(email);
-		return matcher.matches();
-	}
+
+   private static final String EMAIL_PATTERN =
+           "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+           + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+   private static Pattern pattern;
+   private static Matcher matcher;
+
+   static {
+      pattern = Pattern.compile(EMAIL_PATTERN);
+   }
+
+   public static boolean isEmail(String email) {
+      if (StringUtils.isEmpty(email)) {
+         return false;
+      }
+      matcher = pattern.matcher(email);
+      return matcher.matches();
+   }
 }
