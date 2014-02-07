59878eb058416752a63c413b6e1d20d44ccf63bf
dwissk <dominik.wisskirchen@ebf.de>
2014-02-07 13:55:13 +0100

    Fix email regex to accept example@sub.my-domain.com

--- src/main/java/de/ebf/utils/FormUtils.java
+++ src/main/java/de/ebf/utils/FormUtils.java
@@ -7,8 +7,7 @@ import org.apache.commons.lang.StringUtils;
 public class FormUtils {
 
    private static final String EMAIL_PATTERN =
-           "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-           + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+           "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
    private static Pattern pattern;
    private static Matcher matcher;
 
