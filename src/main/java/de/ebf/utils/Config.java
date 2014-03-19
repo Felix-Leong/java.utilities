9b2d5dd21ce89fbaec0692e069845b77eba490e5
xiaodongzhang <xiaodong.zhang@ebf.de>
2014-03-19 09:49:55 +0100

    add basePath parameter to support jRebel

--- src/main/java/de/ebf/utils/Config.java
+++ src/main/java/de/ebf/utils/Config.java
@@ -5,17 +5,29 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
 public class Config {
-	
-	public static PropertiesConfiguration instance;
-	
-	static{
-		try {
-			AbstractConfiguration.setDefaultListDelimiter(';');
-			instance = new PropertiesConfiguration();
-			instance.setEncoding("UTF-8");
-			instance.load("LocalSettings.properties");
-		} catch (ConfigurationException e) {
-			throw new RuntimeException("Unable to read required config file "+e);
-		}
-	}	
+
+    public static PropertiesConfiguration instance;
+
+    static {
+        load();
+    }
+
+    public static void load(String basePath) {
+        try {
+            AbstractConfiguration.setDefaultListDelimiter(';');
+            instance = new PropertiesConfiguration();
+            instance.setEncoding("UTF-8");
+            if(basePath!=null){
+                instance.setBasePath(basePath);
+            }
+            instance.load("LocalSettings.properties");
+        } catch (ConfigurationException e) {
+            throw new RuntimeException("Unable to read required config file " + e);
+        }
+    }
+    
+    public static void load() {
+        load(null);
+    }
+    
 }
