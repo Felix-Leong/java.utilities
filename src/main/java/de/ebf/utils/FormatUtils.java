8d96c674e93926594321e868011d5b1214866466
dwissk <dominik.wisskirchen@ebf.de>
2014-03-21 12:17:36 +0100

    fix NPE

--- src/main/java/de/ebf/utils/FormatUtils.java
+++ src/main/java/de/ebf/utils/FormatUtils.java
@@ -15,7 +15,7 @@ public class FormatUtils {
 	private static final Logger log = Logger.getLogger(FormatUtils.class);
 	
 	public static String readableFileSize(Long size) {
-	    if(size <= 0) return "0";
+	    if(size == null || size <= 0) return "0";
 	    final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
 	    int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
 	    return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
