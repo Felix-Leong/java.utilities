7cdf6d95d0aa3150baa4036f96ab08338a6e34ec
dwissk <dominik.wisskirchen@ebf.de>
2014-04-11 11:30:23 +0200

    make Bundle also read from basemessages.properties

--- src/main/java/de/ebf/utils/Bundle.java
+++ src/main/java/de/ebf/utils/Bundle.java
@@ -1,13 +1,19 @@
 package de.ebf.utils;
 
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Bundle {
-	
-	private static ResourceBundle bundle = ResourceBundle.getBundle("messages");
 
-	public static String getString(String string) {
-		return bundle.getString(string);
-	}
+    private static final ResourceBundle baseMessagesBundle  = ResourceBundle.getBundle("basemessages");
+    private static final ResourceBundle messagesBundle      = ResourceBundle.getBundle("messages");
+    
+    public static String getString(String string) {
+        try {
+            return baseMessagesBundle.getString(string);
+        } catch (MissingResourceException ex){
+            return messagesBundle.getString(string);
+        }
+    }
 
 }
