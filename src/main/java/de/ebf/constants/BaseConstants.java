e4dd3ca429f6891c6a80538fa501dc49b3c20e09
dwissk <dominik.wisskirchen@ebf.de>
2014-05-26 15:41:07 +0200

    change base constants

--- src/main/java/de/ebf/constants/BaseConstants.java
+++ src/main/java/de/ebf/constants/BaseConstants.java
@@ -14,7 +14,8 @@ public class BaseConstants {
     
     public static final String UTF8                                     = "UTF-8";
     public static final String SESSION_USER                             = "user";
-    public static final String HTTP_RESPONSE_HEADER_MSG                 = "msg";
+    public static final String HTTP_RESPONSE_HEADER_ERROR_MSG           = "ErrorMsg";
+    public static final String HTTP_RESPONSE_HEADER_ERROR_SRC      = "ErrorSrc";
     public static final String HTTP_REQUEST_HEADER_CLIENT_OS            = "clientOS";
     public static final String HTTP_REQUEST_HEADER_CLIENT_VERSION       = "clientVersion";
     public static final String HTTP_REQUEST_HEADER_CLIENT_DEVICE        = "clientDevice";
