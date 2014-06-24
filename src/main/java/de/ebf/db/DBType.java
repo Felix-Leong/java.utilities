5683ba4b8c4346515df2c45ad2395f4191cb349c
dwissk <dominik.wisskirchen@ebf.de>
2014-06-24 11:45:06 +0200

    add table and column sql queries

--- src/main/java/de/ebf/db/DBType.java
+++ src/main/java/de/ebf/db/DBType.java
@@ -11,21 +11,38 @@ package de.ebf.db;
  */
 public enum DBType {
 
-    MySQL("MySQL", "com.mysql.jdbc.Driver", "org.hibernate.dialect.MySQLDialect", "jdbc:mysql://", "select 1 from dual"), 
-    MSSQL("Miscrosoft SQL Server", "net.sourceforge.jtds.jdbc.Driver", "org.hibernate.dialect.SQLServerDialect", "jdbc:jtds:sqlserver://", "SELECT GETDATE()"); 
+    MySQL("MySQL",
+          "com.mysql.jdbc.Driver",
+          "org.hibernate.dialect.MySQLDialect",
+          "jdbc:mysql://",
+          "SELECT 1 from dual",
+          "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA=DATABASE();",
+          "SELECT COLUMN_NAME, DATA_TYPE FROM information_schema.COLUMNS WHERE TABLE_SCHEMA=DATABASE() AND TABLE_NAME='{0}';"),
+    
+    MSSQL("Miscrosoft SQL Server",
+          "net.sourceforge.jtds.jdbc.Driver",
+          "org.hibernate.dialect.SQLServerDialect",
+          "jdbc:jtds:sqlserver://",
+          "SELECT GETDATE()",
+          "SELECT TABLE_NAME FROM information_schema.TABLES;",
+          "SELECT TABLE_NAME, DATA_TYPE FROM information_schema.COLUMNS WHERE TABLE_NAME='{0}';"); 
 
     private String label;
     private String driverClass;
     private String dialect;
     private String urlPrefix;
     private String testQuery;
+    private String tableQuery;
+    private String columnQuery;
 
-    DBType(String label, String driverClass, String dialect, String urlPrefix, String testQuery) {
+    DBType(String label, String driverClass, String dialect, String urlPrefix, String testQuery, String tableQuery, String columnQuery) {
         this.label = label;
         this.driverClass = driverClass;
         this.dialect = dialect;
         this.urlPrefix = urlPrefix;
         this.testQuery = testQuery;
+        this.tableQuery = tableQuery;
+        this.columnQuery = columnQuery;
     }
 
     public String getDialect() {
@@ -48,4 +65,19 @@ public enum DBType {
         return testQuery;
     }
 
+    public String getTableQuery() {
+        return tableQuery;
+    }
+
+    public void setTableQuery(String tableQuery) {
+        this.tableQuery = tableQuery;
+    }
+
+    public String getColumnQuery() {
+        return columnQuery;
+    }
+
+    public void setColumnQuery(String columnQuery) {
+        this.columnQuery = columnQuery;
+    }
 }
