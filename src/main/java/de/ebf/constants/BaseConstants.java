e4cb79b7c0b2416c00cdb83beebc3b94ac0bddd7
dwissk <dominik.wisskirchen@ebf.de>
2014-07-02 14:53:48 +0200

    change http header fields to include X-prefix

--- src/main/java/de/ebf/constants/BaseConstants.java
+++ src/main/java/de/ebf/constants/BaseConstants.java
@@ -14,11 +14,11 @@ public class BaseConstants {
     
     public static final String UTF8                                     = "UTF-8";
     public static final String SESSION_USER                             = "user";
-    public static final String HTTP_RESPONSE_HEADER_ERROR_MSG           = "ErrorMsg";
-    public static final String HTTP_RESPONSE_HEADER_ERROR_SRC      = "ErrorSrc";
-    public static final String HTTP_REQUEST_HEADER_CLIENT_OS            = "clientOS";
-    public static final String HTTP_REQUEST_HEADER_CLIENT_VERSION       = "clientVersion";
-    public static final String HTTP_REQUEST_HEADER_CLIENT_DEVICE        = "clientDevice";
-    public static final String HTTP_REQUEST_HEADER_API_VERSION          = "apiVersion";
+    public static final String HTTP_RESPONSE_HEADER_ERROR_MSG           = "X-Error-Msg";
+    public static final String HTTP_RESPONSE_HEADER_ERROR_SRC           = "X-Error-Src";
+    public static final String HTTP_REQUEST_HEADER_CLIENT_OS            = "X-Client-OS";
+    public static final String HTTP_REQUEST_HEADER_CLIENT_VERSION       = "X-Client-Version";
+    public static final String HTTP_REQUEST_HEADER_CLIENT_DEVICE        = "X-Client-Device";
+    public static final String HTTP_REQUEST_HEADER_API_VERSION          = "X-Api-Version";
     
 }
